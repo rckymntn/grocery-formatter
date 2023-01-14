fb307a0069cbd20467caed670cad2521b4ae3015
Logan Raffkind <logan.raffkind@gmail.com>
2023-01-13 19:22:31 -0800

    Added some comments

--- src/index.js
+++ src/index.js
@@ -1,33 +1,46 @@
 let delimiter = "###";
 
 window.onload = () => {
+
+    // What happens when the sort button is clicked
     let sortButtonElement = document.querySelector("#sortButton");
+    // Sanity check
     if (sortButtonElement) {
         sortButtonElement.addEventListener("click", () => {
             let inputText = document.querySelector("#inputTextArea").value;
+            // Ensure output textarea is clear
             document.querySelector("#outputTextArea").value = "";
+            // Write a grouped list to the output textarea
             document.querySelector("#outputTextArea").value = group(inputText);
         });
     }
 
+    // What happens when the copy button is clicked
     let copyButtonElement = document.querySelector("#copyButton");
+    // Sanity check
     if (copyButtonElement) {
         copyButtonElement.addEventListener("click", () => {
+            // Copy text in output textarea to clipboard
             navigator.clipboard.writeText(document.querySelector("#outputTextArea").value);
         });
     }
 
+    // What happens when the clear button is clicked
     let clearButtonElement = document.querySelector("#clearButton");
+    // Sanity check
     if (clearButtonElement) {
         clearButtonElement.addEventListener("click", () => {
             if (confirm("Are you sure you would like to clear your list?")) {
+                // Clear input and output textareas
                 document.querySelector("#inputTextArea").value = "";
                 document.querySelector("#outputTextArea").value = "";
             }
         });
     }
 
+    // What happens when a new delimiter for groups is selected
     let selectDelimiterElement = document.querySelector("#delimiterSelect");
+    // Sanity check
     if (selectDelimiterElement) {
         selectDelimiterElement.addEventListener("change", () => {
             delimiter = selectDelimiterElement.value;
